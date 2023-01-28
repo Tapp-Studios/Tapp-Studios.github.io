a4f34a0341f79ffa2dc393e32b5b1d9839554af7
Tapp-Studios <perrys741@protonmail.com>
2023-01-27 18:09:43 -0700

    b

--- Register.js
+++ Register.js
@@ -1,6 +1,6 @@
 function Register(){
     // Selecting the input element and get its value 
-    var inputVal = document.getElementById("usename").value;
+    var inputVal = document.getElementById("username").value;
     window.alert("By");
     
     // Displaying the value
