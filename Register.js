1e8173db363665d442e80c57389b866c342346ee
Tapp-Studios <perrys741@protonmail.com>
2023-01-27 17:59:21 -0700

    m

---
+++ Register.js
@@ -0,0 +1,8 @@
+function Register(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("usename").value;
+    window.alert("By");
+    
+    // Displaying the value
+
+}
