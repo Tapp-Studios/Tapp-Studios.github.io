c64f8e9b3334c50b02691f44d0f55f0051494c1a
Tapp Studios <perrys3177@gmail.com>
2023-02-02 09:06:54 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -1,5 +1,6 @@
 var users =["Spencer","John"]
 alert(users);
+alert("It works?");
 function compareInput() {
   const input = document.getElementById("input").value;
   for (let i = 0; i < users.length; i++){
