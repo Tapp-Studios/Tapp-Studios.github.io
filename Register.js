a558f44765fe3ba619a7430362c939df65b90b28
Tapp Studios <perrys3177@gmail.com>
2023-02-02 08:54:05 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -2,10 +2,11 @@ let text = '{ "employees" : [' +
 '{ "firstName":"John" , "lastName":"Doe" },' +
 '{ "firstName":"Anna" , "lastName":"Smith" },' +
 '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-const parse = JSON.parse(text);
+const parsed = JSON.parse(text);
+alert(parsed);
 function compareInput() {
   const input = document.getElementById("input").value;
-  for (const key in parse){
+  for (const key in parsed){
   if(obj.hasOwnProperty(key)){
     alert("Match found!");
   }
