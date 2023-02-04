3d85b1061cf3fea367905abae9fd40292905cf31
Tapp-Studios <perrys741@protonmail.com>
2023-02-04 11:40:22 -0700

    Updated Webpage Layout

--- Register.js
+++ Register.js
@@ -1,12 +1,12 @@
-import data from './Data.json';
-console.log(data);
-var users =["Spencer","John"]
-alert(users);
-alert("It works?");
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+var unames =["Spencer","John"]
+alert(unames);
 function compareInput() {
   const input = document.getElementById("input").value;
-  for (let i = 0; i < users.length; i++){
-    if(input==users[i]){
+  for (let i = 0; i < unames.length; i++){
+    if(input==unames[i]){
       alert("Match found!");
     }
   }
