9bc924b8f1ac0e6f286faa4ff82adc87337b99ef
Tapp-Studios <perrys741@protonmail.com>
2023-02-06 16:58:30 -0700

    Updated Login Code

--- Register.js
+++ Register.js
@@ -2,12 +2,25 @@ fetch('./data.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
 var unames =["Spencer","John"]
-alert(unames);
-function compareInput() {
-  const input = document.getElementById("input").value;
-  for (let i = 0; i < unames.length; i++){
-    if(input==unames[i]){
-      alert("Match found!");
+var passes =["password","example"]
+var e=0
+function compareInputs() {
+    const input = document.getElementById("userin").value;
+    const passin = document.getElementById("passin").value;
+    for (let i = 0; i < unames.length; i++){
+        if(input==unames[i]){
+            for (let j = 0; j < passes.length; j++){
+                if(passin==passes[i]){
+                    window.location.replace("https://tapp-studios.github.io");
+                } else {
+                    e=e+1
+                }
+            }
+        } else {
+            e=e+1
+        }
     }
-  }
+    if(e>0){
+      alert("Username or Password incorrect");
+    }
 }
