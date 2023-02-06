ed240e03415f3c876fc40791f2c626f143dc2a28
Tapp Studios <perrys3177@gmail.com>
2023-02-06 12:48:35 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -7,10 +7,8 @@ var passes =["password","example"]
 function compareInputs() {
     const input = document.getElementById("userin").value;
     const passin = document.getElementById("passin").value;
-    alert("JS started");
     for (let i = 0; i < unames.length; i++){
         if(input==unames[i]){
-            alert("Username found");
             for (let j = 0; j < passes.length; j++){
                 if(passin==passes[i]){
                     window.location.replace("https://tapp-studios.github.io");
