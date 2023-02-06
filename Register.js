9a6b00c06c406716f06094c55b9c46be01fede58
Tapp Studios <perrys3177@gmail.com>
2023-02-06 12:00:55 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -2,12 +2,12 @@ fetch('./data.json')
     .then((response) => response.json())
     .then((json) => console.log(json));
 var unames =["Spencer","John"]
-alert(unames);
 function compareInput() {
   const input = document.getElementById("input").value;
   for (let i = 0; i < unames.length; i++){
     if(input==unames[i]){
       alert("Match found!");
+      window.location.replace("https://tapp-studios.github.com");
     }
   }
 }
