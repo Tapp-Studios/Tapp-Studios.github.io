adb36eb9f0d3fe22fc4e07d76189b52bb4a84a17
Tapp Studios <perrys3177@gmail.com>
2023-02-06 12:44:47 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -6,10 +6,15 @@ var passes =["password","example"]
 
 function compareInputs() {
     const input = document.getElementById("userin").value;
+    const passin = document.getElementById("passin").value;
     for (let i = 0; i < unames.length; i++){
         if(input==unames[i]){
             alert("Username found");
-            window.location.replace("https://tapp-studios.github.io");
+            for (let j = 0; j < passes.length; j++){
+                if(passin==passes[i]){
+                    window.location.replace("https://tapp-studios.github.io");
+                }
+            }
         }
     }
 }
