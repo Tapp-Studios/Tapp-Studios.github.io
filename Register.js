87d540faa93a043167f9f6ca32d2ec749079f01b
Tapp Studios <perrys3177@gmail.com>
2023-02-06 12:19:57 -0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -3,17 +3,17 @@ fetch('./data.json')
     .then((json) => console.log(json));
 var unames =["Spencer","John"]
 var passes =["password","example"]
+
 function compareInputs() {
-  const input = document.getElementById("userin").value;
-  const input = document.getElementById("passin").value;
-  for (let i = 0; i < unames.length; i++){
-      if(input==unames[i]){
-        window.location.replace("https://tapp-studios.github.io");
+    const input = document.getElementById("userin").value;
+    const input = document.getElementById("passin").value;
+    for (let i = 0; i < unames.length; i++){
+        if(input==unames[i]){
             for (let i = 0; i < passes.length; i++){
                 if(input==passes[i]){
                     window.location.replace("https://tapp-studios.github.io");
                 }
             }
-      }
-  }
+        }
+    }
 }
